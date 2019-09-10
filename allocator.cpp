f048a1f843d28d29ea09a16001792fd5cdb7249b
An N <trypic@gmail.com>
2019-09-11 00:48:47 +0300

    Add files via upload

--- allocator.cpp
+++ allocator.cpp
@@ -2,116 +2,103 @@
 #include <vector>
 #include <map>
 
-template<typename T>
+template<typename T, size_t reserve_size = 10>
 struct logging_allocator {
    using value_type = T;
+   size_t allocated_size;
+   size_t created_size;
+   void* p;
+
+   template<typename U>
+   struct rebind {
+     using other = logging_allocator<U>;
+   };
+
+   logging_allocator():allocated_size(0), created_size(0){}
+
    T *allocate(std::size_t n){
-     auto p = std::malloc(n * sizeof(T));
-     std::cout << "allocate n = " << n << '\n';
-     if (!p)
-       throw std::bad_alloc();
-     return reinterpret_cast<T *>(p);
+          std::cout << "call allocate n = " << n << '\n';
+     if (created_size + n > reserve_size){
+         throw;
+     }
+
+     void* ptr;
+     if (allocated_size == 0){
+       ptr = std::malloc(reserve_size * sizeof(T));
+       std::cout << "reserve n = " << reserve_size << '\n';
+       if (!ptr)
+         throw std::bad_alloc();
+
+       allocated_size = reserve_size;
+       p = ptr;
+     }
+     else {
+       std::cout << "call allocate but not allocate " << n << " elements\n";
+       ptr = p;
+       p += n;
+     }
+     return reinterpret_cast<T *>(ptr);
    }
    void deallocate(T *p, std::size_t n){
      std::cout << "deallocate n = " << n << std::endl;
      std::free(p);
+     allocated_size = 0;
    }
    template<typename U, typename ...Args>
    void construct(U *p, Args &&...args){
      new(p) U(std::forward<Args>(args)...);
      std::cout << "construct\n";
+     created_size += sizeof...(Args);
    }
    void destroy(T *p){
      std::cout << "destroy\n";
      p->~T();
+     --created_size;
    }
 };
 
 template<class T, class Allocator = std::allocator<T>>
-  class SimpleList {
-    struct Node {
-      Node* next;
-      T current;
-    };
+  class CustomContainer {
 public:
-  SimpleList():head(nullptr){}
-  ~SimpleList(){
-    Node* next = head;
-    while (next){
-      Node* deleteNode = next;
-      next = next->next;
-      delete deleteNode;
+  CustomContainer() = default;
+  ~CustomContainer(){
+    if (size()){
+      T* fist_p = data.at(0);
+      const size_t size = data.size();
+      for (auto i: data){
+        alloc.destroy(i);
+      }
+      alloc.deallocate(fist_p, size);
     }
   }
-  void push_back(T l){
-    Node *n = new Node();
-    n->current = l;
-    n->next = head;
-    head = n;
+  void push_back(T item){
+    T* p_item = alloc.allocate(1);
+    alloc.construct(p_item, item);
+    data.push_back(p_item);
   }
-   T pop_first(){
-     Node *n = head;
-     T ret = n->current;
-     head = head->next;
-     delete n;
-     return ret;
-   }
+
+  T get(size_t n){
+    if (n >= size())
+      throw std::out_of_range("");
+    else
+      return *data.at(n);
+  }
+
+  size_t size(){
+    return data.size();
+  }
+
 private:
-  Node *head;
+  std::vector<T*> data;
+  Allocator alloc;
 };
 
 
-// template<typename T>
-// struct logging_allocator {
-//     using value_type = T;
-//
-//     using pointer = T*;
-//     using const_pointer = const T*;
-//     using reference = T&;
-//     using const_reference = const T&;
-//
-//     template<typename U>
-//     struct rebind {
-//         using other = logging_allocator<U>;
-//     };
-//
-//     logging_allocator() = default;
-//     ~logging_allocator() = default;
-//
-//     template<typename U>
-//     logging_allocator(const logging_allocator<U>&) {
-//
-//     }
-//
-//     T *allocate(std::size_t n) {
-//         std::cout << "allocate: [n = " << n << "]" << std::endl;
-//         auto p = std::malloc(n * sizeof(T));
-//         if (!p)
-//             throw std::bad_alloc();
-//         return reinterpret_cast<T *>(p);
-//     }
-//
-//     void deallocate(T *p, std::size_t n) {
-//         std::cout << "deallocate: [n  = " << n << "] " << std::endl;
-//         std::free(p);
-//     }
-//
-//     template<typename U, typename ...Args>
-//     void construct(U *p, Args &&...args) {
-//         std::cout << "construct" << std::endl;
-//         new(p) U(std::forward<Args>(args)...);
-//     }
-//
-//     void destroy(T *p) {
-//         std::cout << "destroy" << std::endl;
-//         p->~T();
-//     }
-// };
 
 int main(int, char *[]) {
    auto v = std::vector<int, logging_allocator<int>>{};
    v.reserve(5);
-   for (int i = 0; i < 6; ++i) {
+   for (int i = 0; i < 5; ++i) {
        std::cout << "vector size = " << v.size() << std::endl;
        v.emplace_back(i);
        std::cout << std::endl;
@@ -132,10 +119,10 @@ int main(int, char *[]) {
       }
 
     std::cout << "My list" << std::endl;
-     SimpleList<int> l;
+    CustomContainer<int, logging_allocator<int>> l;
     l.push_back(3);
     l.push_back(5);
-    std::cout << l.pop_first() << std::endl;
+    std::cout << "oh hi" << std::endl;
 
     std::cout << "\n**** end of main ****\n" << std::endl;
 
